742c3d1f9f568965fc064e879ae805e92fb210a0
sayali <hackyourfuturedk@HackYourFutures-MacBook-Air.local>
2023-07-01 16:27:55 +0200

    week2 hw file updated

--- src/backend/app.js
+++ src/backend/app.js
@@ -21,10 +21,79 @@ app.use(cors());
 
 router.use("/meals", mealsRouter);
 
+router.use("/reservations", reservationRouter);
+
+// Route to get future meals sorted by ID
+app.get("/future-meals", async (req, res) => {
+  try {
+    const meals = await knex
+      .select()
+      .from("meal")
+      .where("when", ">", knex.fn.now());
+    console.log(meals);
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json("500. Internal Server Error");
+  }
+});
+
+// Respond with all meals in the past (relative to the when datetime)
+app.get("/past-meals", async (req, res) => {
+  try {
+    const meals = await knex
+      .select()
+      .from("meal")
+      .where("when", "<", knex.fn.now())
+      .orderBy("id");
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json("500. Internal Server Error");
+  }
+});
+// Respond with all meals sorted by ID
+app.get("/all-meals", async (req, res) => {
+  try {
+    const meals = await knex
+      .select("title", "description")
+      .from("meal")
+      .orderBy("id");
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json("500. Internal Server Error");
+  }
+});
+
+//Respond with the first meal (meaning with the minimum id)
+app.get("/first-meal", async (req, res) => {
+  try {
+    const meal = await knex
+      .select("title", "description")
+      .from("meal")
+      .orderBy("id")
+      .first();
+    res.status(200).json(meal);
+  } catch (error) {
+    res.status(500).json("500. Internal Server Error");
+  }
+});
+//Respond with the last meal (meaning with the maximum id)
+app.get("/last-meal", async (req, res) => {
+  try {
+    const meal = await knex
+      .select("title", "description")
+      .from("meal")
+      .orderBy("id", "desc")
+      .limit(1);
+    res.status(200).json(meal);
+  } catch (error) {
+    res.status(500).json("500. Internal Server Error");
+  }
+});
+
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
-  throw "API_PATH is not set. Remember to set it in your .env file"
+  throw "API_PATH is not set. Remember to set it in your .env file";
 }
 
 // for the frontend. Will first be covered in the react class
